88bab3d6b146d57c6e16679130b94c636aa1f325
Petya Buchkova <petya.buchkova@gmail.com>
2021-11-04 14:58:12 +0100

    WIP on ZenodoAPI#

--- common/helpers/http.go
+++ common/helpers/http.go
@@ -1,15 +1,23 @@
 package helpers
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 )
 
-func httpRequest(method, url string) (*http.Response) {
-	req, err := http.NewRequest(method, url, nil)
-
+func httpRequest(method, url string, payload io.Reader) ([]byte) {
+	req, err := http.NewRequest(method, url, payload)
+	if(method == "POST") {
+		req.Header.Add("Content-Type", "application/json")
+	}
 	handleError(err)
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	res, err := http.DefaultClient.Do(req)
 	handleError(err)
-	return resp
+	defer res.Body.Close()
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(res)
+	fmt.Println(string(body))
+	return body
 }
--- common/helpers/zenodoAPI.go
+++ common/helpers/zenodoAPI.go
@@ -2,21 +2,38 @@ package helpers
 
 import (
 	"fmt"
+	"strings"
+
+	"github.com/m7shapan/njson"
 )
 
-type HTTPResponse struct {
-	message  string `json:"message"`
-	status   int    `json:"status"`
+type ZENODOBucketResponse struct {
+	Bucket  string `njson:"links.bucket"`
 }
 
 const (
-	ZENODO_API = "https://zenodo.org/api/deposit/depositions"
+	ZENODO_API = "https://zenodo.org/api/"
 )
 
 
 func ZenodoAPI() {
-	const zenodoToken = getEnvVariable("ZENODO_API_KEY")
-	response := httpRequest("GET", ZENODO_API);
-	fmt.Println(response);
+	zenodoToken := getEnvVariable("ZENODO_API_KEY")
+	endpoint := ZENODO_API + "deposit/depositions?access_token=" + zenodoToken
+	payload := strings.NewReader("{}")
+	response := httpRequest("POST",endpoint,payload)
+	res,_ := UnmarshalResponse(response)
+	buckerURL := res.Bucket
+	fmt.Println(buckerURL)
+
+	// TODO: use bucket URL to upload the file in the bucket
+	// bucketURL/fileName?access_token=zenodoToken
+
+
 	return
+}
+
+func UnmarshalResponse(data []byte) (ZENODOBucketResponse, error) {
+	var r ZENODOBucketResponse
+	err := njson.Unmarshal(data, &r)
+	return r, err
 }

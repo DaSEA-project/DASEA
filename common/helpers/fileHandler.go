06b5852e6c84f713ebee1d8b7d8bda6de52d4861
Kasper Olsen <kasper@kols.dk>
2021-11-11 10:04:27 +0100

    trimming

--- common/helpers/fileHandler.go
+++ common/helpers/fileHandler.go
@@ -1,5 +1,11 @@
 package helpers
 
+import (
+	"encoding/csv"
+	"os"
+	"strings"
+)
+
 // Parse json to struct type
 func ParseJSON() int {
 	return 42
@@ -13,4 +19,28 @@ func ParseYAML() int {
 // Write json to CSV file
 func JSONToCSV() int {
 	return 42
+}
+
+func WriteToCsv(data [][]string, filePath string) {
+	tokens := strings.Split(filePath, "/")
+	path := strings.Join(tokens[:len(tokens)-1], "/")
+
+	err := os.MkdirAll(path, 0777)
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.Create(filePath)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	for _, value := range data {
+		writer.Write(value)
+	}
 }

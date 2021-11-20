006687bb6629ee4f9bfcad7fc33206d4f74cbd57
heyjoakim <joakimhey@gmail.com>
2021-11-20 14:26:07 +0100

    Finished CSV for all of conan

--- common/helpers/fileHandler.go
+++ common/helpers/fileHandler.go
@@ -83,6 +83,74 @@ func WriteLineToCsv(model models.Package, filePath string) {
 	writer.Write(model.GetValues())
 }
 
+func WriteCsvVersion(model models.Version, filePath string) {
+	tokens := strings.Split(filePath, "/")
+	path := strings.Join(tokens[:len(tokens)-1], "/")
+
+	// TODO make simple, stupid
+	if _, err := os.Stat(filePath); errors.Is(err, os.ErrNotExist) {
+		file, err := os.Create(filePath)
+		if err != nil {
+			panic(err)
+		}
+		writer := csv.NewWriter(file)
+		writer.Write(model.GetKeys())
+		writer.Flush()
+		file.Close()
+	}
+
+	err := os.MkdirAll(path, 0777)
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write(model.GetValues())
+}
+
+func WriteCsvDependency(model models.CsvDependency, filePath string) {
+	tokens := strings.Split(filePath, "/")
+	path := strings.Join(tokens[:len(tokens)-1], "/")
+
+	// TODO make simple, stupid
+	if _, err := os.Stat(filePath); errors.Is(err, os.ErrNotExist) {
+		file, err := os.Create(filePath)
+		if err != nil {
+			panic(err)
+		}
+		writer := csv.NewWriter(file)
+		writer.Write(model.GetKeys())
+		writer.Flush()
+		file.Close()
+	}
+
+	err := os.MkdirAll(path, 0777)
+	if err != nil {
+		panic(err)
+	}
+	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	writer.Write(model.GetValues())
+}
+
 func checkFile(filename string) error {
 	_, err := os.Stat(filename)
 	if os.IsNotExist(err) {

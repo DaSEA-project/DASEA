697e6ac755fbc2d8b510d25e100d888d6a17bad3
HelgeCPH <ropf@itu.dk>
2022-01-25 14:40:37 +0100

    Converted Cargo miner to DB dump miner to speed it up

--- dasea/cargo.py
+++ dasea/cargo.py
@@ -1,20 +1,12 @@
 import os
-import csv
-import json
 import logging
+import tarfile
 import requests
-import subprocess
+import pandas as pd
+from glob import glob
+from pathlib import Path
 from datetime import datetime
-from dataclasses import dataclass
-from dasea.datamodel import Package, Version, Dependency, Kind
-
-# TODO: Switch mining to database dump: https://crates.io/data-access
-# TODO: Add request header as specified here https://crates.io/policies
-# Based on the documentation form here:
-CRATES_URL = "https://crates.io/api/v1/crates?page={page_idx}&per_page=100"
-CRATE_URL = "https://crates.io/api/v1/crates/{name}"
-VERSION_URL = "https://crates.io/api/v1/crates/{name}/{version}/dependencies"
-INDEX_FILE = "data/tmp/cargo/crates_index.json"
+from dasea.datamodel import Kind
 
 
 TODAY = datetime.today().strftime("%m-%d-%Y")
@@ -22,6 +14,9 @@ PKGS_FILE = f"data/out/cargo/cargo_packages_{TODAY}.csv"
 VERSIONS_FILE = f"data/out/cargo/cargo_versions_{TODAY}.csv"
 DEPS_FILE = f"data/out/cargo/cargo_dependencies_{TODAY}.csv"
 DONE_FILE = "data/out/cargo/done"
+
+CARGO_DB_DUMP_URL = "https://static.crates.io/db-dump.tar.gz"
+TMP_DIR = "data/tmp/cargo/"
 
 
 logging.basicConfig(
@@ -31,217 +26,189 @@ logging.getLogger("urllib3.connectionpool").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
 
 
-@dataclass
-class CargoPackage(Package):
-    description: str
-    homepage: str
-    repository: str
+def _collect_packages(crates_csv):
+    df = pd.read_csv(crates_csv)
+    df.fillna("", inplace=True)
+    df["pkgman"] = "Cargo"
+
+    df = df[["id", "name", "pkgman", "description", "homepage", "repository"]]
+    df.to_csv(PKGS_FILE, index=False)
 
 
-@dataclass
-class CargoVersion(Version):
-    author_nick: str
-    created_at: str
-    updated_at: str
-    no_downloads: int
+def _collect_versions(crates_csv, versions_csv, users_csv):
+    cdf = pd.read_csv(crates_csv)
+    cdf = cdf[["id", "name", "description", "homepage", "repository"]]
+    cdf.fillna("", inplace=True)
+    cdf.rename(columns={"id": "pkg_idx"}, inplace=True)
 
-
-@dataclass
-class CargoDependency(Dependency):
-    kind_str: str
-    optional: bool
-    target: str
-
-
-def _collect_pkg_metadata():
-    if os.path.isfile(INDEX_FILE):
-        with open(INDEX_FILE) as fp:
-            crates_info = json.load(fp)
-        return crates_info
-
-    LOGGER.info("Collecting package index from the web, it takes some time...")
-    page_idx = 1
-    r = requests.get(CRATES_URL.format(page_idx=page_idx))
-    r.raise_for_status()
-
-    crates_info = r.json()["crates"]
-    next_page = r.json()["meta"]["next_page"]
-    while next_page:
-        if page_idx % 50 == 0:
-            LOGGER.info(page_idx)
-        r = requests.get(CRATES_URL.format(page_idx=page_idx))
-        crates_info += r.json()["crates"]
-        next_page = r.json()["meta"]["next_page"]
-        page_idx += 1
-
-    with open(INDEX_FILE, "w") as fp:
-        json.dump(crates_info, fp)
-    return crates_info
-
-
-def _parse_crates(crates_docs):
-    keep = [
-        # "id",  # Names and ids are always the same, so keep only one of them
-        "name",
-        "description",
-        "homepage",
-        "repository",
-        "downloads",
-        "created_at",
-        "updated_at",
+    vdf = pd.read_csv(versions_csv)
+    vdf = vdf[
+        [
+            "crate_id",
+            "id",
+            "license",
+            "num",
+            "published_by",
+            "created_at",
+            "updated_at",
+            "downloads",
+        ]
     ]
-    crates_infos = []
-    for c in crates_docs:
-        crate_info = {}
-        for col in keep:
-            crate_info[col] = c[col]
-        crates_infos.append(crate_info)
-    return crates_infos
+    vdf.rename(columns={"crate_id": "pkg_idx"}, inplace=True)
+    vdf = pd.merge(cdf, vdf, how="inner", on="pkg_idx")
+    vdf.rename(
+        columns={
+            "id": "idx",
+            "downloads": "no_downloads",
+            "published_by": "owner_id",
+            "num": "version",
+        },
+        inplace=True,
+    )
+
+    udf = pd.read_csv(users_csv)
+    udf.rename(
+        columns={
+            "id": "owner_id",
+            "gh_login": "author_github_nick",
+            "name": "author",
+        },
+        inplace=True,
+    )
+    vdf = pd.merge(udf, vdf, how="right", on="owner_id")
+
+    vdf["maintainer"] = ""
+
+    vdf = vdf[
+        [
+            "idx",
+            "pkg_idx",
+            "name",
+            "version",
+            "license",
+            "description",
+            "homepage",
+            "repository",
+            "author",
+            "maintainer",
+            "author_github_nick",
+            "created_at",
+            "updated_at",
+            "no_downloads",
+        ]
+    ]
+    vdf.fillna("", inplace=True)
+    vdf.to_csv(VERSIONS_FILE, index=False)
 
 
-def get_remote_data(url, pkg_name, version_num=None):
-    if version_num:
-        url = url.format(name=pkg_name, version=version_num)
-    else:
-        url = url.format(name=pkg_name)
-    while True:
-        # Or better go for a Retry object???
-        # https://stackoverflow.com/questions/23013220/max-retries-exceeded-with-url-in-requests
-        try:
-            r = requests.get(url)
-            break
-        except requests.exceptions.ConnectionError:
-            LOGGER.info("Connection refused. I will try again in 5 secs...")
-            sleep(5)
-            continue
-        except requests.exceptions.ConnectionResetError:
-            LOGGER.info("Connection refused. I will try again in 5 secs...")
-            sleep(5)
-            continue
-        except Exception as e:
-            LOGGER.error(e)
-            sleep(5)
-            continue
+def _collect_dependencies(crates_csv, versions_csv, deps_csv):
+    cdf = pd.read_csv(crates_csv)
+    cdf = cdf[["id", "name", "description", "homepage", "repository"]]
+    cdf.fillna("", inplace=True)
+    cdf.rename(columns={"id": "pkg_idx"}, inplace=True)
 
-    if not r.ok:
-        if version_num:
-            LOGGER.warning(f"{r.status_code} VERSION {pkg_name} {version_num}")
-        else:
-            LOGGER.warning(f"{r.status_code} PKG {pkg_name}")
+    vdf = pd.read_csv(versions_csv)
+    vdf = vdf[
+        [
+            "crate_id",
+            "id",
+            "license",
+            "num",
+            "published_by",
+            "created_at",
+            "updated_at",
+            "downloads",
+        ]
+    ]
+    vdf.rename(columns={"crate_id": "pkg_idx"}, inplace=True)
+    vdf = pd.merge(cdf, vdf, how="inner", on="pkg_idx")
+    vdf.rename(
+        columns={
+            "id": "version_id",
+            "downloads": "no_downloads",
+            "published_by": "owner_id",
+            "num": "version",
+        },
+        inplace=True,
+    )
 
-    return r
+    ddf = pd.read_csv(deps_csv)
+    ddf = ddf[["crate_id", "kind", "optional", "req", "target", "version_id"]]
+    ddf.rename(
+        columns={
+            "crate_id": "target_idx",
+            "req": "target_version",
+        },
+        inplace=True,
+    )
+    ddf = pd.merge(ddf, vdf[["pkg_idx", "name", "version", "version_id"]], how="left", on="version_id")
 
+    ddf.rename(
+        columns={"name": "source_name", "version_id": "source_idx", "version": "source_version", "kind": "kind_code"},
+        inplace=True,
+    )
 
-def iterative_data_collection(pkgs_lst):
-    dep_kind_map = {
-        "normal": Kind.NORMAL.name,
-        "dev": Kind.DEV.name,
-        "build": Kind.BUILD.name,
-    }
-    pkg_idx_map = {p["name"]: idx for idx, p in enumerate(pkgs_lst)}
+    short_cdf = cdf[["pkg_idx", "name"]]
+    short_cdf.rename(
+        columns={
+            "pkg_idx": "target_idx",
+            "name": "target_name",
+        },
+        inplace=True,
+    )
+    ddf = pd.merge(ddf, short_cdf, how="left", on="target_idx")
 
-    version_idx = 0
+    ddf["kind"] = ""
+    ddf.loc[ddf.kind_code == 0, "kind"] = Kind.NORMAL.name
+    ddf.loc[ddf.kind_code == 1, "kind"] = Kind.BUILD.name
+    ddf.loc[ddf.kind_code == 2, "kind"] = Kind.DEV.name
 
-    with open(PKGS_FILE, "a") as fp, open(VERSIONS_FILE, "a") as fv, open(DEPS_FILE, "a") as fd:
-        pkgs_csv_writer = csv.writer(fp)
-        versions_csv_writer = csv.writer(fv)
-        deps_csv_writer = csv.writer(fd)
+    ddf.loc[ddf.optional == "t", "optional"] = True
+    ddf.loc[ddf.optional == "f", "optional"] = False
 
-        pkgs_csv_writer.writerow(CargoPackage(None, None, None, None, None, None).__dict__.keys())
-        versions_csv_writer.writerow(
-            CargoVersion(
-                None, None, None, None, None, None, None, None, None, None, None, None, None, None
-            ).__dict__.keys()
-        )
-        deps_csv_writer.writerow(
-            CargoDependency(None, None, None, None, None, None, None, None, None, None, None).__dict__.keys()
-        )
-
-        for pkg_idx, pkg_info in enumerate(pkgs_lst):
-            pkg_name = pkg_info["name"]
-
-            pkg = CargoPackage(
-                idx=pkg_idx,
-                name=pkg_name,
-                pkgman="Cargo",
-                description=pkg_info["description"],
-                homepage=pkg_info["homepage"],
-                repository=pkg_info["repository"],
-            )
-            pkgs_csv_writer.writerow(pkg.__dict__.values())
-            fp.flush()
-            os.fsync(fp)
-
-            r = get_remote_data(CRATE_URL, pkg_name)
-            for version_info in r.json()["versions"]:
-                crate = {}
-                if type(version_info["crate"]) == dict:
-                    crate = version_info["crate"]
-                published_by = {}
-                if version_info["published_by"]:
-                    published_by = version_info["published_by"]
-                version_num = version_info["num"]
-                v = CargoVersion(
-                    idx=version_idx,
-                    pkg_idx=pkg_idx_map.get(pkg_name, None),
-                    name=pkg_name,
-                    version=version_num,
-                    license=version_info["license"],
-                    description=crate.get("description", None),
-                    homepage=published_by.get("url", None),
-                    repository=crate.get("repository", None),
-                    author=published_by.get("name", None),  # or shall the login be taken?
-                    maintainer=None,  # There is no such information
-                    author_nick=published_by.get("login", None),
-                    created_at=version_info["created_at"],
-                    updated_at=version_info["updated_at"],
-                    no_downloads=version_info["downloads"],
-                )
-
-                versions_csv_writer.writerow(v.__dict__.values())
-                fp.flush()
-                os.fsync(fv)
-                version_idx += 1
-
-                r = get_remote_data(VERSION_URL, pkg_name, version_num)
-                for dep_info in r.json().get("dependencies", []):
-                    d = CargoDependency(
-                        pkg_idx=pkg_idx_map.get(pkg_name, None),
-                        source_idx=version_idx,
-                        target_idx=pkg_idx_map.get(dep_info["crate_id"], None),
-                        source_name=pkg_name,
-                        target_name=dep_info["crate_id"],
-                        source_version=version_idx,
-                        target_version=dep_info["req"],
-                        kind=dep_kind_map.get(dep_info["kind"], None),
-                        kind_str=dep_info["kind"],
-                        optional=dep_info["optional"],
-                        target=dep_info["target"],
-                    )
-                    deps_csv_writer.writerow(d.__dict__.values())
-                    fp.flush()
-                    os.fsync(fd)
+    ddf = ddf[
+        [
+            "pkg_idx",
+            "source_idx",
+            "target_idx",
+            "source_name",
+            "target_name",
+            "source_version",
+            "target_version",
+            "kind",
+            "optional",
+            "target",
+        ]
+    ]
+    ddf.fillna("", inplace=True)
+    ddf.to_csv(DEPS_FILE, index=False)
 
 
 def mine():
-    pkg_metadata_dict = _collect_pkg_metadata()
-    pkgs_lst = _parse_crates(pkg_metadata_dict)
-    LOGGER.info(f"There should be {len(pkgs_lst)} pkgs in the end.")
+    LOGGER.info("Downloading database dump...")
+    dump_file = Path(TMP_DIR, "db_dump.tar.gz")
+    # with open(dump_file, "wb") as fp:
+    #     r = requests.get(CARGO_DB_DUMP_URL)
+    #     fp.write(r.content)
+    # LOGGER.info("Extracting database dump...")
+    # with tarfile.open(dump_file) as fp:
+    #     fp.extractall(TMP_DIR)
 
-    iterative_data_collection(pkgs_lst)
+    # os.remove(dump_file)
+    dataset_dir = glob(TMP_DIR + "*")[0]  # TODO: is it always the last?
+    dataset_dir = Path(dataset_dir, "data")
 
-    # Poor mans way to communicate to remote host that mining is complete
-    cmd = "hostname -I"
-    r = subprocess.run(cmd, shell=True, capture_output=True, text=True)
-    host_ip = r.stdout.split()[0]
-    with open(DONE_FILE, "w") as fp:
-        fp.write(f"http://{host_ip}:8080/{PKGS_FILE}\n")
-        fp.write(f"http://{host_ip}:8080/{VERSIONS_FILE}\n")
-        fp.write(f"http://{host_ip}:8080/{DEPS_FILE}")
+    crates_csv = Path(dataset_dir, "crates.csv")
+    versions_csv = Path(dataset_dir, "versions.csv")
+    users_csv = Path(dataset_dir, "users.csv")
+    deps_csv = Path(dataset_dir, "dependencies.csv")
 
-    cmd = "nohup python -m http.server 8080 --directory /vagrant/data/out/cargo/ &"
-    subprocess.run(cmd, shell=True)
-    # os.remove(INDEX_FILE)
+    LOGGER.info("Converting packages to DaSEA...")
+    _collect_packages(crates_csv)
+    LOGGER.info("Converting versions to DaSEA...")
+    _collect_versions(crates_csv, versions_csv, users_csv)
+    LOGGER.info("Converting dependencies to DaSEA...")
+    _collect_dependencies(crates_csv, versions_csv, deps_csv)
 
 
 if __name__ == "__main__":

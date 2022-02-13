fd484bcfc5cdd45bce3b7e52cc25af618879eb94
Petya Buchkova <petya.buchkova@gmail.com>
2022-02-13 17:53:24 +0100

    Fixed for macOS

--- bin/get_netbsd_pkgsrc.sh
+++ bin/get_netbsd_pkgsrc.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-vagrant ssh netbsd9 --command "cd /vagrant/ && python -m core.ports"
+vagrant ssh netbsd9 --command "cd /vagrant/ && python -m core.miners.ports"
+
 # The NetBSD VM does not support virtualbox two-way synced folders, therefore manual collection of results
 # Requires the Vagrant SCP plugin to be installed
 vagrant scp 'netbsd9:/vagrant/data/out/ports/netbsd9/*packages*.csv' data/out/ports/netbsd9/

72362b5d812543ccc187312b84df0f82cdc89843
Kasper Olsen <kasper@kols.dk>
2022-02-18 14:25:02 +0100

    updated prereqs file

--- bin/install_prerequisites.sh
+++ bin/install_prerequisites.sh
@@ -1,41 +1,38 @@
 
-## Apt update and upgrade
-# sudo apt update -y
+# Apt update and upgrade
+sudo apt update -y
 
-## Install Python
-# sudo apt install python3.9 -y
+# Install Python
+sudo apt install python3.9 -y
 
-## Install Pip
-# sudo apt install python3-pip -y
+# Install Pip
+sudo apt install python3-pip -y
 
-## Install Virtualenv
-# sudo pip3 install virtualenv
+# Install Virtualenv
+sudo pip3 install virtualenv
 
-##  Make a folder for virtual enviornments
-# mkdir env
+#  Make a folder for virtual enviornments
+mkdir env
 
-## Install Poetry
-# curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+# Install Poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 
-## Source profile
+# Source profile
 source ~/.profile
 
-## Create virtual enviornment
-# virtualenv --python=/usr/bin/python3.9 env/dasea
+# Create virtual enviornment
+virtualenv --python=/usr/bin/python3.9 env/dasea
 
-## Activate the virtual enviornment
-# source env/dasea/bin/activate
+# Activate the virtual enviornment
+source env/dasea/bin/activate
 
-## TODO: Install virtualbox
+# TODO: Install virtualbox
 
-## Enter dasea project
+# Enter dasea project
 cd dasea-tool
 
-## Setup Python version
-# poetry env use /usr/bin/python3.9
+# Setup Python version
+poetry env use /usr/bin/python3.9
 
-## Install dependencies
-# poetry install
-
-## Run FPM miner
-poetry run dasea mine fpm
+# Install dependencies
+poetry install

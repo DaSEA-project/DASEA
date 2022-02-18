4793fa7274719dd9cefa3a11d9051c4e929d21be
Kasper Olsen <kasper@kols.dk>
2022-02-18 13:49:06 +0100

    updated provision to use snapshot

--- bin/install_prerequisites.sh
+++ bin/install_prerequisites.sh
@@ -1,9 +1,9 @@
 
 ## Apt update and upgrade
-sudo apt update -y
+# sudo apt update -y
 
 ## Install Python
-sudo apt install python3.9 -y
+# sudo apt install python3.9 -y
 
 ## Install Pip
 # sudo apt install python3-pip -y
@@ -15,10 +15,10 @@ sudo apt install python3.9 -y
 # mkdir env
 
 ## Install Poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
+# curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 
 ## Source profile
-source ~/.profile
+# source ~/.profile
 
 ## Create virtual enviornment
 # virtualenv --python=/usr/bin/python3.9 env/dasea
@@ -32,7 +32,10 @@ source ~/.profile
 cd dasea-tool
 
 ## Setup Python version
-poetry env use /usr/bin/python3.9
+# poetry env use /usr/bin/python3.9
 
 ## Install dependencies
-poetry install
+# poetry install
+
+## Run FPM miner
+poetry run dasea mine fpm

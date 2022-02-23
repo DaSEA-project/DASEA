5095915cdcea9ed514e5a50cbfe9309794df33ab
Petya Buchkova <petya.buchkova@gmail.com>
2022-02-23 15:08:31 +0100

    WIP om create_dataset_parallel.sh

---
+++ bin/create_dataset_parallel.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# poetry run dasea mine cargo
+# poetry run dasea mine alire
+# poetry run dasea mine fpm
+# poetry run dasea mine vcpkg
+# poetry run dasea mine homebrew
+# poetry run dasea mine chromebrew
+
+vagrant up ubuntu2104
+vagrant up freebsd11
+# vagrant up openbsd69
+# vagrant up netbsd9
+# vagrant up ubuntu2104oneway
+
+
+vagrant ssh ubuntu2104 --command "cd /vagrant/ && poetry run dasea mine conan" &
+bash bin/get_freebsd_ports.sh &
+
+vagrant destroy -f ubuntu2104
+vagrant destroy -f freebsd11
+
+# vagrant up ubuntu2104
+# vagrant ssh ubuntu2104 --command "cd /vagrant/ && poetry run dasea mine conan"
+# vagrant destroy -f ubuntu2104
+
+
+# vagrant up freebsd11
+# bash bin/get_freebsd_ports.sh
+# vagrant destroy -f freebsd11
+
+# vagrant up openbsd69
+# # bash bin/get_openbsd_ports.sh
+# vagrant destroy -f openbsd69
+
+# # vagrant up netbsd9
+# bash bin/get_netbsd_pkgsrc.sh # Takes a really long time, 2-3h
+# vagrant destroy -f netbsd9
+
+# # vagrant up ubuntu2104oneway
+# bash bin/get_nimble_pkgs.sh
+# vagrant destroy -f ubuntu2104oneway

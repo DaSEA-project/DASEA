89f606521cc263f54461b2633bd288d381772054
Petya Buchkova <petya.buchkova@gmail.com>
2022-02-24 10:58:35 +0100

    Updated create_release.yml

--- bin/create_dataset_parallel.sh
+++ bin/create_dataset_parallel.sh
@@ -7,17 +7,24 @@
 # poetry run dasea mine homebrew
 # poetry run dasea mine chromebrew
 
-vagrant up ubuntu2104
-vagrant up freebsd11
+#vagrant up ubuntu2104
+#vagrant up freebsd11
 # vagrant up openbsd69
 # vagrant up netbsd9
 # vagrant up ubuntu2104oneway
 
 
-nohup vagrant ssh ubuntu2104 --command "cd /vagrant/ && poetry run dasea mine conan" &
-nohup bash bin/get_freebsd_ports.sh &
+# nohup vagrant ssh ubuntu2104 --command "cd /vagrant/ && poetry run dasea mine conan" &
+# nohup bash bin/get_freebsd_ports.sh &
 
-#nohup sh -c 'while ps -p $0 > /dev/null; do sleep 10; done && mv $1 $1_done' vagrant destroy -f ubuntu2104 &
+nohup poetry run dasea mine cargo &
+CARGO_ID=$!
+echo $CARGO_ID
+nohup poetry run dasea mine alire &
+ALIRE_ID=$!
+echo $ALIRE_ID
+
+# nohup sh -c 'while ps -p $CARGO_ID > /dev/null; echo "Process is running"; do sleep 10; done && mv $1 $1_done' vagrant destroy -f ubuntu2104 &
 #nohup sh -c 'while ps -p $1 > /dev/null; do sleep 10; done && mv $2 $2_done' vagrant destroy -f freebsd11 &
 
 

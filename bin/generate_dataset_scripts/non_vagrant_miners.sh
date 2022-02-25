0eee5dd25680385febe3f4fca67dbe0f71dab1fc
Petya Buchkova <petya.buchkova@gmail.com>
2022-02-25 12:34:57 +0100

    Updated non_vagrant_miners.sh

--- bin/generate_dataset_scripts/non_vagrant_miners.sh
+++ bin/generate_dataset_scripts/non_vagrant_miners.sh
@@ -7,7 +7,7 @@ echo $SSH_KEYS
 doctl compute droplet create --image 102629764 --size s-4vcpu-8gb --region ams3 --wait --ssh-keys 33315393 DASEA-tool-miner-1
 
 ## Get the droplet's IP address
-IP_ADDRESS=$(doctl compute droplet list --format PublicIPv4Address | grep -v null)
+IP_ADDRESS=$(doctl compute droplet get --format "PublicIPv4" --no-header DASEA-tool-miner-1)
 
 ## Add to the hosts file
 ssh-keyscan -H $IP_ADDRESS >> ~/.ssh/known_hosts
@@ -16,10 +16,11 @@ ssh-keyscan -H $IP_ADDRESS >> ~/.ssh/known_hosts
 exec ssh-agent bash
 ssh-add ~/.ssh/dasea
 
-## SSH into the droplet
-# doctl compute ssh DASEA-tool-miner-1 --ssh-command "source ~/.profile && git clone https://github.com/dependulum/DASEA.git && cd DASEA && poetry install && poetry run dasea mine alire"
+## Copy ssh keys into droplet
+scp -i ~/.ssh/dasea ~/.ssh/miner1.pub root@$IP_ADDRESS:/root/.ssh/
 
-doctl compute ssh DASEA-tool-miner-1 --ssh-command "source ~/.profile && cd DASEA && poetry install && poetry run dasea mine alire && scp -r ./data/out root@157.245.70.200:/root/DASEA/data/out"
+## SSH into the droplet
+doctl compute ssh DASEA-tool-miner-1 --ssh-command "source ~/.profile && git clone git@github.com:dependulum/DASEA.git && cd DASEA && poetry install && poetry run dasea mine alire && scp -i ~/.ssh/miner1 -r ./data/out root@157.245.70.200:/root/DASEA/data/out"
 
 ## Execute all miners sequentially
 

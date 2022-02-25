862781ab511a78c7479fa5e73d3b7beee6d961ac
heyjoakim <joakimhey@gmail.com>
2022-02-25 14:46:34 +0100

    updated release scripts

--- bin/trigger_dataset_generation.sh
+++ bin/trigger_dataset_generation.sh
@@ -1,17 +1,14 @@
-
-
 ## Execute in parallel with nohup
 nohup bash bin/generate_dataset_scripts/non_vagrant_miners.sh &
 NON_VAGRANT_MINERS_ID=$!
-echo $NON_VAGRANT_MINERS_ID
 
 ## FreeBSD script... for all Vagrant
 
 COUNT=0
 
 # Destroy all machines
-nohup sh -c 'while ps -p $NON_VAGRANT_MINERS_ID > /dev/null; do echo "Non Vagrant Miners process is running" && sleep 300; done && ((COUNT++))'
+nohup sh -c "while ps -p $NON_VAGRANT_MINERS_ID > /dev/null; do echo 'Non Vagrant Miners process is running' && sleep 300; done && $COUNT++"
 
 
 ## Once done, trigger release and push to GitHub
-nohup sh -c 'while [ $COUNT -lt 1 ]; do echo "Mining is still running" && sleep 3000; done && bash bin/release_dataset.sh'
+nohup sh -c "while [ $COUNT -lt 1 ]; do echo 'Mining is still running' && sleep 3000; done && bash bin/release_dataset.sh"

9f930c3312e863f52544abbb2725f748f4e7d76e
Kasper Olsen <kasper@kols.dk>
2022-03-03 12:47:42 +0100

    Cleaned up pipelines. Added destroy miner droplet

--- bin/release/generate_dataset_scripts/non_vagrant_miners.sh
+++ bin/release/generate_dataset_scripts/non_vagrant_miners.sh
@@ -31,21 +31,8 @@ cd DASEA && poetry install &&
 poetry run dasea mine alire &&
 scp -o StrictHostKeyChecking=no -i ~/.ssh/id_rsa -r ./data/out root@steve.dasea.org:/root/DASEA/data"
 
-## SSH into the droplet
-# echo "SSH into the droplet..."
-# doctl compute ssh DASEA-tool-miner-1 --ssh-command "
-# ssh-keyscan -H 157.245.70.200 >> ~/.ssh/known_hosts &&
-# source ~/.profile && git clone https://github.com/dependulum/DASEA.git &&
-# cd DASEA && poetry install &&
-# poetry run dasea mine alire &&
-# poetry run dasea mine cargo &&
-# poetry run dasea mine chromebrew &&
-# poetry run dasea mine fpm &&
-# poetry run dasea mine homebrew &&
-# poetry run dasea mine vcpkg &&
-# scp -i ~/.ssh/id_rsa -r ./data/out root@157.245.70.200:/root/DASEA/data"
+## Destroy droplet
+echo "Destroying droplet..."
+doctl compute droplet delete DASEA-tool-miner-1 --force
 
 echo "Completed..."
-
-
-## Destroy droplet

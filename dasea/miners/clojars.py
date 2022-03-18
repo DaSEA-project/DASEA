1a3fb9c784485b5fab92ddd099432f2de8bc209e
Petya Buchkova <petya.buchkova@gmail.com>
2022-03-18 15:47:13 +0100

    Cleaned up clojars miner

--- dasea/miners/clojars.py
+++ dasea/miners/clojars.py
@@ -2,14 +2,14 @@ import sys
 import logging
 import requests
 from datetime import datetime
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 from dasea.common.datamodel import Package, Version, Dependency, Kind
 from dasea.common.utils import _serialize_data
-from tqdm import tqdm
 
 # Based on the documentation form here:
 # https://github.com/clojars/clojars-web/wiki/Data
 
-# TODO: https://clojars.org/search?q=reveal&format=json
 CLOJARS_REGISTRY = "https://repo.clojars.org/all-jars.clj"
 CLOJARS_PACKAGE_URL =  "https://clojars.org/api/artifacts/{jar_name}"
 CLOJARS_VERSION_URL = "https://clojars.org/{pkg_name}/versions/{version}"
@@ -24,14 +24,6 @@ PKGS_FILE = f"data/out/clojars/clojars_packages_{TODAY}.csv"
 VERSIONS_FILE = f"data/out/clojars/clojars_versions_{TODAY}.csv"
 DEPS_FILE = f"data/out/clojars/clojars_dependencies_{TODAY}.csv"
 
-
-import os
-import csv
-import operator
-import itertools
-from bs4 import BeautifulSoup
-
-
 logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s", datefmt="%Y-%m-%d %H:%M"
 )
@@ -43,11 +35,10 @@ def _collect_pkg_registry():
     LOGGER.info("Collecting Clojars registry...")
     r = requests.get(CLOJARS_REGISTRY)
     if not r.ok:
-        raise IOError("Cannot download FPM registry.")
+        raise IOError("Cannot download Clojars registry.")
 
     jars = set([])
     for line in r.text.splitlines():
-      # Remove the brackets
       jar_name, _ = line[1:-1].split()
       jars.add(jar_name)
 
@@ -86,7 +77,6 @@ def _collect_versions_with_dependencies(metadata_dict, pkg_idx_map):
                 v = Version(
                     idx=version_idx,
                     pkg_idx=pkg_idx,
-                    # name=version_info["name"],
                     name=pkg_name,
                     version=version_number,
                     license=version_info["license"],
@@ -101,7 +91,6 @@ def _collect_versions_with_dependencies(metadata_dict, pkg_idx_map):
                 v = Version(
                     idx=version_idx,
                     pkg_idx=pkg_idx,
-                    # name=version_info["name"],
                     name=pkg_name,
                     version=version_number,
                     license=None,
@@ -115,11 +104,8 @@ def _collect_versions_with_dependencies(metadata_dict, pkg_idx_map):
 
             try:
                 for d in version_info["dependencies"]:
-                    # Get source ID
                     target_name, target_version = d.text.split()
-                    # if the TARGET_ID cannot be found it is a reference to a
-                    # Maven package
-                    # What to do with them?
+                    # IMPORTANT: if the TARGET_ID cannot be found it is a reference to a Maven package
                     target_idx = pkg_idx_map.get(target_name, None)
                     source_pkg_idx = pkg_idx_map.get(pkg_name, None)
 
@@ -164,16 +150,6 @@ def scrape_package_version_info(pkg_name, version_number):
         author = soup.find("ul", {"id": "jar-sidebar"}).find("li").find("a").text
     except:
         author = ""
-    # try:
-    #     date = soup.find("ul", {"id": "jar-sidebar"}).find("span").get("title")
-    # except:
-    #     date = ""
-    # try:
-    #     source_version = (
-    #         soup.find("ul", {"id": "jar-sidebar"}).find("span", {"class": "commit-url"}).find("a").get("href")
-    #     )
-    # except:
-    #     source_version = ""
     try:
         dependencies = soup.find("ul", {"id": "dependencies"}).find_all("a")
     except:
@@ -186,66 +162,9 @@ def scrape_package_version_info(pkg_name, version_number):
         "repository": sources_url,
         "author": author,
         "dependencies": dependencies
-        # Clojar specific columns
-        # "date": date,
-        # "source_version": source_version,
     }
 
     return version_data_with_deps
-
-
-
-
-# def _collect_dependencies(metadata_dict, pkg_idx_map):
-#     deps = []
-#     version_idx = 0
-#     for idx, (pkg_name, data) in enumerate(metadata_dict.items()):
-#         package_versions = []
-#         for version, version_info in data.items():
-#             # Add version number to list, to check if version appears more than once in package registry
-#             if version in package_versions:
-#                 continue
-#             package_versions.append(version_info["version"])
-
-#             source_pkg_idx = pkg_idx_map.get(pkg_name, None)
-#             deps_decl = version_info.get("dependencies", {})
-#             dev_deps_decl = version_info.get("dev-dependencies", {})
-#             if deps_decl == None:
-#                 # sometimes there is an explicit None value stored
-#                 deps_decl = {}
-#             if dev_deps_decl == None:
-#                 dev_deps_decl = {}
-
-#             for dep_name, dep_info in deps_decl.items():
-#                 d = Dependency(
-#                     pkg_idx=source_pkg_idx,
-#                     source_idx=version_idx,
-#                     target_idx=pkg_idx_map.get(
-#                         dep_name, ""
-#                     ),  # There are packages provided as dependencies, which do not exist on the registry, but which are referred to and downloaded from, e.g., Github
-#                     source_name=version_info["name"],
-#                     target_name=dep_name,
-#                     source_version=version_info["version"],
-#                     target_version=dep_info.get("tag", "") or dep_info.get("rev", ""),
-#                     kind=Kind.BUILD.name,
-#                 )
-#                 deps.append(d)
-#             for dep_name, dep_info in dev_deps_decl.items():
-#                 d = Dependency(
-#                     pkg_idx=source_pkg_idx,
-#                     source_idx=version_idx,
-#                     target_idx=pkg_idx_map.get(dep_name, ""),
-#                     source_name=version_info["name"],
-#                     target_name=dep_name,
-#                     source_version=version_info["version"],
-#                     target_version=dep_info.get("tag", "") or dep_info.get("rev", ""),
-#                     kind=Kind.DEV.name,
-#                 )
-#                 deps.append(d)
-#             version_idx += 1
-
-#     return deps
-
 
 def mine():
     try:
@@ -253,9 +172,7 @@ def mine():
     except IOError as e:
         LOGGER.error(str(e))
         sys.exit(1)
-    # pkg_names = list(metadata_dict.keys())
-    print(f"Found {len(metadata_dict)} packages in registry.")
-    # print(metadata_dict)
+
     LOGGER.info("Creating metadata_dict packages...")
     pkg_idx_map, packages_lst = _collect_packages(metadata_dict)
     LOGGER.info("Creating DaSEA versions with dependencies...")

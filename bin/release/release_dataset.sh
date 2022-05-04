17c16e0894b98ffa6f623fd16bb70250d106cf22
Petya Buchkova <petya.buchkova@gmail.com>
2022-05-04 12:11:41 +0200

    Small fix for dataset ordering in homepage

--- bin/release/release_dataset.sh
+++ bin/release/release_dataset.sh
@@ -3,4 +3,8 @@
 datasetFileName=$(poetry run dasea release)
 echo "$datasetFileName"
 
-poetry run dasea push --sandbox --no-verify "data/out/$datasetFileName"
+# Release in Sandbox https://sandbox.zenodo.org/
+# poetry run dasea push --sandbox --no-verify "data/out/$datasetFileName"
+
+# Release in Zenodo https://zenodo.org/
+poetry run dasea push --no-verify "data/out/$datasetFileName"

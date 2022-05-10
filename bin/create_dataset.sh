6a26053220cebb50d89648f6d2ca7e2983da8860
Petya Buchkova <petya.buchkova@gmail.com>
2022-05-10 13:04:56 +0200

    Small fixes

--- bin/create_dataset.sh
+++ bin/create_dataset.sh
@@ -21,7 +21,7 @@ bash bin/get_openbsd_ports.sh
 vagrant destroy -f openbsd69
 
 vagrant up netbsd9
-bash bin/get_netbsd_pkgsrc.sh # Takes a really long time, 2-3h
+bash bin/get_netbsd_pkgsrc.sh
 vagrant destroy -f netbsd9
 
 vagrant up ubuntu2104oneway
